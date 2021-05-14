990cb732049407cc8752f0aed8109b53a91c7a3d
ok97 <omkhawshi0@gmail.com>
2021-05-14 16:57:43 +0530

    Added: Insert Value

--- Payroll Service.sql
+++ Payroll Service.sql
@@ -14,3 +14,27 @@ use Payroll_Service
 --- Understand the SQL data types to be used for the table
 --- Note: SQL Queries as case insensitive
 
+create Table employee_payroll
+(
+id int  NOT NULL Identity(1,1) primary key,
+name varchar(20)  NOT NULL,
+salary float NOT NULL,
+startdate Date NOT NULL);
+
+select * from employee_payroll -- Show table
+
+
+---UC3:- Ability to create employee payroll data in the payroll service database as part of CURD Operation.
+-- Use payroll_service database in MSSQL Client.
+---Use INSERT INTO employee_payroll Query to create employees payroll data into the employee_payroll table.
+
+
+ insert into employee_payroll(name,salary,startdate)
+ values('Jack','2500','2021-05-13');
+
+insert into employee_payroll VALUES('Omprakash', 50000, '2021-05-14');
+
+INSERT INTO employee_payroll ( name, salary,startdate)
+VALUES ('Om', '5000','2008-11-11');
+select * from employee_payroll -- Show table
+
